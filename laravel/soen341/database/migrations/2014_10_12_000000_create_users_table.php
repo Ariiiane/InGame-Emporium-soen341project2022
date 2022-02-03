b587a3fbbb07de8149e901349c9acf207c99e4ea
Jay <roebuck.jayc@gmail.com>
2022-02-03 13:26:55 -0500

    Added database migration files for all db tables in schema

--- laravel/soen341/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/soen341/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('email', 200)->unique();
+            $table->string('password', 200);
+            $table->string('role', 200);
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('postal_code', 50);
+            $table->string('province', 50);
+            $table->string('address', 200);
         });
     }
 
