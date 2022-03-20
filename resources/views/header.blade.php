093666780c1507156aa638ad23a109048c8434ec
Ariane <ariane_drouin@outlook.com>
2022-03-20 09:01:03 -0400

    add the link to the cart in the header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -28,6 +28,16 @@
                     <a class="nav-link">Cart</a>
                 </li>
             </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{url('')}}/cart">Cart</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
             <div>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     @if (Route::has('login'))
