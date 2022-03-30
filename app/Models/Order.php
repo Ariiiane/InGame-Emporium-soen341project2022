44fbf7a09044e28f4b30569e134add57e92f1a34
Jay <roebuck.jayc@gmail.com>
2022-03-30 15:39:46 -0400

    Added Order model

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'customer_id',
+        'order_date',
+        'delivery_date',
+        'delivery_address',
+        'shipping_speed',
+        'billing_address',
+        'payment_card_number',
+        'payment_card_expiry',
+        'payment_card_cvv',
+        'payment_card_name',
+    ];
+}
