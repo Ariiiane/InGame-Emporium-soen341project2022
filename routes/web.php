74a2bb70fc41337127e35bef087c3ba02ec538a1
hilary-ostof <hilary.fotso22@gmail.com>
2022-04-07 16:25:35 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
+<<<<<<< HEAD
+=======
+use Illuminate\Support\Facades\Log;
+>>>>>>> parent of f79b602e (Merge branch 'main' of https://github.com/Ariiiane/InGame-Emporium-soen341project2022)
 
 /*
 |--------------------------------------------------------------------------
@@ -41,6 +45,25 @@ Route::get('/browsing', [ProductController::class, 'get_products']);
 
 Route::resource('products', ProductController::class);
 
+<<<<<<< HEAD
+=======
+// Chloe for Auth
+//Route::get('user-list', [AuthController::class, 'index'])->name('user.list');
+//Route::get('login', [AuthController::class, 'login'])->name('login');
+//Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
+//Route::get('registration', [AuthController::class, 'registration'])->name('register');
+//Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
+//Route::get('dashboard', [AuthController::class, 'dashboard']);
+//Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+
+//Hilary for managing profile
+Route::get('/buyer', [AuthenticatedSessionController::class,'display']);
+Route::get('/buyer', [RegisteredUserController::class,'display']);
+Route::post('edit', [UserController::class, 'editUser'])->name('edit');
+
+
+>>>>>>> parent of f79b602e (Merge branch 'main' of https://github.com/Ariiiane/InGame-Emporium-soen341project2022)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
