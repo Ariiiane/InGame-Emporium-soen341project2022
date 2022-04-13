b9b12707173298ce8fc93fb2dd6777f562ed3996
Aryan <ashirazi@hrwize.com>
2022-04-12 21:13:46 -0400

    second attempt at resolving conflicts

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\FileUploadController;
+
 use Illuminate\Support\Facades\Log;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,8 +38,13 @@ Route::get('/buyer', function () {
 });
 
 Route::get('/seller', function () {
-    return view('seller');
+    return view('/seller');
 });
+
+Route::get('/edit', function () {
+    return view('edit');
+});
+
 
 Route::post('/seller/uploaded_file', [FileUploadController::class, 'fileUploadPost']);
 Route::get('/seller/UploadAds', [ProductController::class, 'get_products_for_dropwdown']);
@@ -45,6 +57,27 @@ Route::delete('/cart/{cart}', [CartController::class, 'destroy'])->name('cart.de
 Route::get('/browsing', [ProductController::class, 'get_products']);
 
 Route::resource('products', ProductController::class);
+
+Route::get('/checkout', [CheckoutController::class, 'index']);
+Route::post('/order_confirmation', [CheckoutController::class, 'success']);
+
+
+
+// Chloe for Auth
+//Route::get('user-list', [AuthController::class, 'index'])->name('user.list');
+//Route::get('login', [AuthController::class, 'login'])->name('login');
+//Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
+//Route::get('registration', [AuthController::class, 'registration'])->name('register');
+//Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
+//Route::get('dashboard', [AuthController::class, 'dashboard']);
+//Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+
+//Hilary for managing profile
+Route::get('/buyer', [AuthenticatedSessionController::class,'display']);
+Route::get('/buyer', [RegisteredUserController::class,'display']);
+Route::post('edit', [UserController::class, 'editUser'])->name('edit');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
